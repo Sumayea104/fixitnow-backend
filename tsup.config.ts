b491e3bb60780a382edae8bcc5b0ce6ded88a15a
Sumayea Rahman <shorbonam104@gmail.com>
2026-07-29 06:21:42 +0600

    fix: vercel deployment

---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/server.ts'], 
+  format: ['cjs'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  minify: false,
+  target: 'node20',
+  splitting: false,
+  outDir: 'dist',
+  external: [
+    '@prisma/client',
+    'bcryptjs',
+    'jsonwebtoken',
+    'stripe',
+    'sslcommerz-lts',
+  ],
+});
