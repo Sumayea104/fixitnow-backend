6aa82b5a619b9df684618c789f5301888aab19cf
Sumayea Rahman <shorbonam104@gmail.com>
2026-07-29 06:46:23 +0600

    fix: explicit dts false in tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/server.ts'], 
   format: ['cjs'],
-  dts: true,
+  dts: false,
   sourcemap: true,
   clean: true,
   minify: false,
